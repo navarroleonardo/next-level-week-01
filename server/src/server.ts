6818610396b36f31c6cdaa7b251d4320381f4a2c
navarroleonardo <abreunavarro.leonardo@gmail.com>
2020-06-02 10:34:56 -0300

    praticando rotas com insomnia

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,45 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = [
+    'Leonardo',
+    'Diego',
+    'Marjori'
+]
+
 app.get('/users', (req, res) => {
-    console.log('Listagem de usuários');
-    res.json([
-        'Leonardo',
-        'Diego',
-        'Marjori'
-    ]);
+
+    const search = String(req.query.search);
+
+    const filteredUsers = search ? users.filter(usuario => usuario.includes(search)) : users;
+
+    return res.json(filteredUsers);
+
+});
+
+app.get('/users/:id', (req, res) => {
+
+    let id = Number(req.params.id);
+
+    const user = users[id];
+
+    return res.json(user);
+
+})
+
+app.post('/users', (req, res) => {
+
+    const { nome, email } = req.body;
+
+    const user = {
+        nome,
+        email
+    };
+
+    return res.json(user);
 });
 
 app.listen(3000);
