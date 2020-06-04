8901fbb76a8dbf730c20b452a1412af3d1f7b0aa
navarroleonardo <abreunavarro.leonardo@gmail.com>
2020-06-04 18:34:58 -0300

    fim aula 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,45 +1,14 @@
 import express, { response } from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(routes);
 
-const users = [
-    'Leonardo',
-    'Diego',
-    'Marjori'
-]
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-app.get('/users', (req, res) => {
-
-    const search = String(req.query.search);
-
-    const filteredUsers = search ? users.filter(usuario => usuario.includes(search)) : users;
-
-    return res.json(filteredUsers);
-
-});
-
-app.get('/users/:id', (req, res) => {
-
-    let id = Number(req.params.id);
-
-    const user = users[id];
-
-    return res.json(user);
-
-})
-
-app.post('/users', (req, res) => {
-
-    const { nome, email } = req.body;
-
-    const user = {
-        nome,
-        email
-    };
-
-    return res.json(user);
-});
-
-app.listen(3000);
+app.listen(3333);
